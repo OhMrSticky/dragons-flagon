bee73f454b132bb9d5a65e2faf855d45eb822d25
Smith, Emily <Emily.Smith@Staples.com>
2018-08-18 16:20:48 -0700

    header styles and some home content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        Welcome to the Dragon's Flagon!
+        <div className="title">Welcome to the Dragon's Flagon!</div>
+        <p>Our D&amp;D Campaign page!</p>
       </div>
     )
   }
