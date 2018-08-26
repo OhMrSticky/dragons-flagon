5bc5158eacb0aed5d820bab5ae4e0d51b8226f9c
Smith, Emily <Emily.Smith@Staples.com>
2018-08-26 09:33:33 -0700

    quest component and data

---
+++ src/components/QuestData.js
@@ -0,0 +1,17 @@
+export const quests = [
+  {
+    title: "Quest of the Spaghetti Monster",
+    active: true,
+    contact: "Tino the Master Chef",
+    rank: 1,
+    reward: "Unlimited free lunches",
+    description: "Find and destroy the run away giant ball of spaghetti that has become a life form seeking destruction of it's primary enemy, humans."
+  }, {
+    title: "The angry Elf",
+    active: false,
+    contact: "Her highness princess Elfarina",
+    rank: 2,
+    reward: "Her highness will look upon your contenance with less than disgust for a moment",
+    description: "Her highness has been dumped by her boyfriend Casanoelfa, the scoundrel. Go find him and insult him mightly."
+  }
+]
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Main.css'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
-import Quests from './Quests'
+import QuestsPage from './QuestsPage'
 
 export default class Main extends React.Component {
   render() {
@@ -10,7 +10,7 @@ export default class Main extends React.Component {
       <main className="container">
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/quests' component={Quests} />
+          <Route path='/quests' component={QuestsPage} />
         </Switch>
       </main>
     )
